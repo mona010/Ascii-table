b4f51630a816bc5fe4674914966b231f22bd5ac5
mona010 <36086043+mona010@users.noreply.github.com>
2018-03-26 10:56:14 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0 ; i <= 255 ; i++){
+        cout << "Number is : " << i << " Ascii is : " << char(i) << endl ;
+    }
+    return 0;
+}
